57d98f8b0939e9d882a49878406b2063d3278691
dukekunyura <dukekunyura@gmail.com>
2023-02-15 15:37:02 +0300

    add Map

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,6 +42,30 @@ public class Main {
         System.out.println(autoList.get(0));
         autoList.set(0, autoList.get(2));
         System.out.println(autoList.get(0));
+
+        Map<String, Auto> autoMap = new HashMap<>();
+
+        autoMap.put("757575", new Auto("Ford", "hatch", "757575"));
+        autoMap.put(cars[1].getBodyNumber(), cars[1]);
+        autoMap.put(cars[2].getBodyNumber(), cars[2]);
+
+        System.out.println(autoMap.get("757575"));
+
+        Collection<Auto> values = autoMap.values();
+        for (Auto auto : values) {
+            System.out.println("----");
+            System.out.println(auto);
+            System.out.println("----");
+        }
+
+        Set<String> keySet = autoMap.keySet();
+
+        System.out.println(keySet);
+
+        Set<Entry<String, Auto>> entrySet = autoMap.entrySet();
+        for (Entry<String, Auto> entry : entrySet) {
+            System.out.println(entry);
+        }
 
     }
 }
