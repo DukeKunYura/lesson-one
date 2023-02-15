2f22b57508b6152b409fb7fc703e149decc333a6
dukekunyura <dukekunyura@gmail.com>
2023-02-15 12:34:49 +0300

    add List

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Person[] persons = {
-            new Person("Andrey", "Kovalev", "45", "locksmith", "3356 909090"),
-            new Person("Ivan", "Popov", "33", "doctor", "3345 909089"),
-            new Person("Timur", "Privetov", "37", "student", "3367 898980"),
+                new Person("Andrey", "Kovalev", "45", "locksmith", "3356 909090"),
+                new Person("Ivan", "Popov", "33", "doctor", "3345 909089"),
+                new Person("Timur", "Privetov", "37", "student", "3367 898980"),
         };
 
         for (int i = 0; i < persons.length; i++) {
@@ -24,6 +27,16 @@ public class Main {
         System.out.println(cars[1]);
         cars[1] = cars[1].withBodyNumber("404040");
         System.out.println(cars[1]);
+
+        List<Auto> autoList = new ArrayList<>();
+
+        autoList.add(cars[0]);
+        autoList.add(cars[1]);
+        autoList.add(cars[2]);
+
+        System.out.println(autoList.get(0));
+        autoList.set(0, autoList.get(2));
+        System.out.println(autoList.get(0));
 
     }
 }
